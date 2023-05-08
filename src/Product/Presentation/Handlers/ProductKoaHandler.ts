403fb62cc5ad5dbd3129effa31023a27ea94c20f
matiasfeliu92 <matumazparrote@gmail.com>
2023-05-08 12:26:19 -0300

    refactor: update ProductKoaHandler, add get by id, post and put endpoints, and add CategoryKoaHandler with get, get by id, post and put endpoints

--- src/Product/Presentation/Handlers/ProductKoaHandler.ts
+++ src/Product/Presentation/Handlers/ProductKoaHandler.ts
@@ -7,17 +7,24 @@ import AuthorizeKoaMiddleware from 'Auth/Presentation/Middlewares/AuthorizeKoaMi
 import CriteriaPayload from 'Shared/Presentation/Validations/CriteriaPayload';
 import IPaginator from 'Shared/Infrastructure/Orm/IPaginator';
 import ProductTransformer from '../Transformers/ProductTransformer';
+import ProductRepPayload from 'Product/Domain/Payloads/ProductRepPayload';
+import { AuthUser } from 'Auth/Presentation/Helpers/AuthUser';
+import DefaultMessageTransformer from 'Shared/Presentation/Transformers/DefaultMessageTransformer';
+import ResponseMessageEnum from 'Shared/Domain/Enum/ResponseMessageEnum';
+import IdPayload from 'Shared/Presentation/Requests/IdPayload';
+import ProductUpdatePayload from 'Product/Domain/Payloads/ProductUpdatePayload';
+import Permissions from 'Config/Permissions';
 
 const routerOpts: Router.IRouterOptions = {
     prefix: '/api/products'
 };
 
-const ItemKoaHandler: Router = new Router(routerOpts);
+const ProductKoaHandler: Router = new Router(routerOpts);
 const responder: KoaResponder = new KoaResponder();
 const controller: ProductController = new ProductController();
 const config = MainConfig.getInstance().getConfig().statusCode;
 
-ItemKoaHandler.get('/', AuthorizeKoaMiddleware(Permissions.PRDDUCTS_LIST), async(ctx: DefaultContext) =>
+ProductKoaHandler.get('/products', AuthorizeKoaMiddleware(Permissions.PRODUCTS_LIST), async(ctx: DefaultContext) =>
 {
     const data: CriteriaPayload = {
         url: ctx.request.url,
@@ -27,4 +34,36 @@ ItemKoaHandler.get('/', AuthorizeKoaMiddleware(Permissions.PRDDUCTS_LIST), async
     const paginator: IPaginator = await controller.list(data);
 
     await responder.paginate(paginator, ctx, config['HTTP_OK'], new ProductTransformer());
+});
+
+ProductKoaHandler.get('/:id', AuthorizeKoaMiddleware(Permissions.PRODUCTS_SHOW), async(ctx: DefaultContext) =>
+{
+    const product = await controller.getOne(ctx.params);
+
+    void await responder.send(product, ctx, config['HTTP_OK'], new ProductTransformer());
+});
+
+ProductKoaHandler.post('/products', AuthorizeKoaMiddleware(Permissions.PRODUCTS_SAVE), async(ctx: DefaultContext) =>
+{
+    const data: ProductRepPayload = {
+        authUser: AuthUser(ctx),
+        ...ctx.request.body
+    };
+
+    const product = await controller.save(data);
+
+    void await responder.send(product, ctx, config['HTTP_CREATED'], new DefaultMessageTransformer(ResponseMessageEnum.CREATED));
+});
+
+ProductKoaHandler.put('/:id', AuthorizeKoaMiddleware(Permissions.PRODUCTS_UPDATE), async(ctx: DefaultContext) =>
+{
+    const data: ProductUpdatePayload = {
+        id: ctx.params.id,
+        authUser: AuthUser(ctx),
+        ...ctx.request.body
+    };
+
+    const product = await controller.update(data);
+
+    void await responder.send(product, ctx, config['HTTP_CREATED'], new DefaultMessageTransformer(ResponseMessageEnum.UPDATED));
 });
